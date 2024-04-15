82b6b382a8942b004ce157cf09e9cf682b50ac78
Manin <103959963+PZ222@users.noreply.github.com>
2024-04-14 20:23:20 -0600

    Update Practica_Bot.py
    Ingreso las primeras dos regex que usare y que funcionaron bien, hasta cierto punto.
    >expresion_regular_estado = re.compile(r"bien|mal|mas o menos|Bien|Mal|Chidote|Buenardo", re.IGNORECASE)
    
    >expresion_regular_despedida = re.compile(r"gracias|muchas gracias|adios|chido", re.IGNORECASE)l
    
    Funcionaban con bien, mal, mas o menos, pero con lo demás no, seguiré comprobando

--- Tema 2/Tarea 2.2/Practica_Bot.py
+++ Tema 2/Tarea 2.2/Practica_Bot.py
@@ -9,9 +9,10 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-# Expresión regular para detectar mensajes que contienen "Hola"
-expresion_regular = re.compile(r"hello|hi|hey|hola", re.IGNORECASE)
-
+# Expresiones regulares para detectar mensajes
+expresion_regular_hola = re.compile(r"hello|hi|hey|hola", re.IGNORECASE)
+expresion_regular_estado = re.compile(r"bien|mal|mas o menos|Bien|Mal|Chidote|Buenardo", re.IGNORECASE)
+expresion_regular_despedida = re.compile(r"gracias|muchas gracias|adios|chido", re.IGNORECASE)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
@@ -21,20 +22,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
-
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message if it matches the regular expression."""
     message_text = update.message.text
-    if expresion_regular.search(message_text):
-        await update.message.reply_text("¡Hola! ¿Cómo estás? yo soy manin")
+    if expresion_regular_hola.search(message_text):
+        await update.message.reply_text("¡Hola! ¿Cómo estás?")
+    elif expresion_regular_estado.search(message_text):
+        if "bien" in message_text:
+            await update.message.reply_text("¡Que bueno me alegro que de todo vaya muy bien por ti!")
+        elif "mal" in message_text:
+            await update.message.reply_text("¡Que mal, espero que pronto te logres recuperar, recuerda que la vida se conforma de picos de felicidad y tristeza!")
+        elif "mas o menos" in message_text:
+            await update.message.reply_text("Manin no entiende, decide si bien o mal, pq asi no te puedo aconsejar :(")
+    elif expresion_regular_despedida.search(message_text):
+        await update.message.reply_text("¡Espero que todo vaya bien para ti! Te deseo lo mejor chams")
     else:
-        await update.message.reply_text("No entendí tu mensaje chamito.")
-
+        await update.message.reply_text("No entendí tu mensaje.")
 
 def main() -> None:
     """Start the bot."""
@@ -45,7 +52,6 @@ def main() -> None:
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
